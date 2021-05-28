3dabc4bd1275c9c7a6da3de1dd1e4b2f3d1e72d9
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2021-05-27 21:49:26 -0300

    incluindo valores na tabela com JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const Modal = {
 
-    toggle (){
+    toggle() {
         document.querySelector(".modal-overlay").classList.toggle("active")
     }
 
@@ -19,46 +19,84 @@ const Modal = {
         document.querySelector(".modal-overlay").classList.remove("active")
     }
     */
-} 
+}
 
-const transactions = [
-    {
-        id: 1,
-        description:"luz",
-        amount: -50000,
-        date: "23/01/2021"
-    },
+//Dados da tabela
 
-    {
-        id: 2,
-        description:"Website",
-        amount: 500000,
-        date: "23/01/2021"
-    },
-
-    {
-        id: 3,
-        description:"Internet",
-        amount: -20000,
-        date: "23/01/2021"
-    }
-]
-
+// Calculo da Entrada menos Saída e o Total 
 // Eu preciso somar as entradas
 // depois eu preciso somar as saídas
 // remover das entradas o valor da saídas
 // assim, eu terei o total
 
 const Transaction = {
+    all: [{
+            description: "luz",
+            amount: -50000,
+            date: "23/01/2021"
+        },
+
+        {
+            description: "Website",
+            amount: 500000,
+            date: "23/01/2021"
+        },
+
+        {
+            description: "Internet",
+            amount: -20000,
+            date: "23/01/2021"
+
+        }, {
+            description: "App",
+            amount: 200000,
+            date: "23/01/2021"
+        },
+    ],
+
+    add(transaction) {
+        Transaction.all.push(transaction)
+        App.reload()
+    },
+
+    remove(index) {
+        Transaction.all.splice(index, 1)
+        App.reload()
+    },
 
     incomes() {
-        //somar as entradas
+        // somar as entradas
+        let income = 0
+        // pegar todas as transações
+        // para cada transacao
+        Transaction.all.forEach(function (transaction) {
+            // se ela for maior que zero    
+            if (transaction.amount > 0) {
+                // somar a uma variavel e retornar a variavel
+                income = income + transaction.amount
+            }
+        })
+
+        return income
     },
-    expenses(){
+    expenses() {
         //somar as saídas
+        let expense = 0
+        // pegar todas as transações
+        // para cada transacao
+        Transaction.all.forEach(function (transaction) {
+            // se ela for menor que zero    
+            if (transaction.amount < 0) {
+                // somar a uma variavel e retornar a variavel
+                expense = expense + transaction.amount
+            }
+        })
+
+        return expense
     },
     total() {
         //entradas - saídas
+        return Transaction.incomes() + Transaction.expenses()
     }
 }
 
@@ -67,36 +105,94 @@ const Transaction = {
 const DOM = {
     transactionsContainer: document.querySelector("#data-table tbody"),
 
-    addTraansaction (transaction, index){
+    addTransaction(transaction, index) {
         const tr = document.createElement("tr")
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
         DOM.transactionsContainer.appendChild(tr)
-        
+
     },
-   
+
     innerHTMLTransaction(transaction) {
 
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
         const amount = Utils.formatCurrency(transaction.amount)
-        
-        
+
+
         const html = `
             <td class="descripition">${transaction.description}</td>
-            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td><img src="./assets/minus.svg" alt="Remover transação"></td>
          `
-         return html
+        return html
+    }, // Jogar na tela os resultados das somas 
+    updateBalance() {
+        document.getElementById("incomeDisplay")
+            .innerHTML = Utils.formatCurrency(Transaction.incomes())
+
+        document.getElementById("expenseDisplay")
+            .innerHTML = Utils.formatCurrency(Transaction.expenses())
+
+        document.getElementById("totalDisplay")
+            .innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
     }
+
 }
+
+//Formatando o numero para REAL
 
 const Utils = {
-    formatCurrency(value){
+    formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
+
     }
 }
 
-transactions.forEach(function(transaction){
-    DOM.addTraansaction(transaction)
-})
+const Form = {
+    submit(event){
+        event.preventDefault()
+
+        // verificar se todas as informaçoes foram preenchidas
+        // formatar os dados para salvar
+        // salvar
+        // apagar os dados do formulario
+        // modeal feche
+        // atualizar a aplicação
+    }
+}
+
+// Colocando e removendo os dados na tabela
+
+const App = {
+    init() {
+        Transaction.all.forEach(function (transaction) {
+            DOM.addTransaction(transaction)
+        })
+        DOM.updateBalance()
+    },
+
+    reload() {
+        // Limpa todo o codigo com string vazia ""
+        DOM.clearTransactions()
+        App.init()
+    },
+
+}
+
+App.init()
